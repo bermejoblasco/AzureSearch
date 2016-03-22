8a0ebd9396e45f8d4d9c78c6dda795bc6fa4d1d8
Robert Bermejo <pollobull12@hotmail.com>
2016-03-22 14:49:24 +0100

    Example azure search

---
+++ AzureSearch/AzureSearch.cs
@@ -0,0 +1,116 @@
+﻿
+namespace AzureSearch
+{
+    using Microsoft.Azure.Search;
+    using Microsoft.Azure.Search.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+
+    class AzureSearch
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var indexName = "example";
+                SearchServiceClient azureSearchService = new SearchServiceClient(ConfigurationManager.AppSettings["AzureSearchServiceName"], new SearchCredentials(ConfigurationManager.AppSettings["AzureSearchKey"]));
+                SearchIndexClient indexClient = azureSearchService.Indexes.GetClient(indexName);
+                var listBooks = new BookModel().GetBooks();
+
+                if (azureSearchService.Indexes.Exists(indexName))
+                {
+                    azureSearchService.Indexes.Delete(indexName);
+                }
+
+                //Create Index Model
+                Index indexModel = new Index()
+                {
+                    Name = indexName,
+                    Fields = new[]
+                    {
+                        new Field("ISBN", DataType.String) { IsKey = true, IsRetrievable = true, IsFacetable = false },
+                        new Field("Titulo", DataType.String) {IsRetrievable = true, IsSearchable = true, IsFacetable = false },
+                        new Field("Autores", DataType.Collection(DataType.String)) {IsSearchable = true, IsRetrievable = true, IsFilterable = true, IsFacetable = false },
+                        new Field("FechaPublicacion", DataType.DateTimeOffset) { IsFilterable = true, IsRetrievable = false, IsSortable = true, IsFacetable = false },
+                        new Field("Categoria", DataType.String) { IsFacetable = true, IsFilterable= true, IsRetrievable = true }
+
+                    }
+                };
+                //Create Index in AzureSearch
+                var resultIndex = azureSearchService.Indexes.Create(indexModel);
+                //Add documents in our Index
+                azureSearchService.Indexes.GetClient(indexName).Documents.Index(IndexBatch.MergeOrUpload<BookModel>(listBooks));
+                //Search by word
+                Console.WriteLine("{0}", "Searching documents 'Cloud'...\n");
+                Search(indexClient, searchText: "Cloud");
+                //Search all and filter
+                Console.WriteLine("\n{0}", "Filter documents by Autores 'Eugenio Betts'...\n");
+                Search(indexClient, searchText: "*", filter: "Autores / any(t: t eq 'Eugenio Betts')");
+
+                //Search all and order
+                Console.WriteLine("\n{0}", "order by FechaPublicacion\n");
+                Search(indexClient, searchText: "*", order: new List<string>() { "FechaPublicacion" });
+
+                ////Search all and facet
+                Console.WriteLine("\n{0}", "Facet by Categoria \n");
+                Search(indexClient, searchText: "*", facets: new List<string>() { "Categoria" });
+
+                Console.WriteLine("Process Ok");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+            finally
+            {
+                Console.WriteLine("Press any key");
+                Console.ReadKey();
+            }
+        }
+
+        private static void Search(SearchIndexClient indexClient, string searchText, string filter = null, List<string> order = null, List<string> facets = null)
+        {
+            // Execute search based on search text and optional filter
+            var sp = new SearchParameters();
+
+            //Add Filter
+            if (!String.IsNullOrEmpty(filter))
+            {
+                sp.Filter = filter;
+            }
+
+            //Order
+            if(order!=null && order.Count>0)
+            {
+                sp.OrderBy = order;
+            }
+            
+            //facets
+            if (facets != null && facets.Count > 0)
+            {
+                sp.Facets = facets;
+            }
+
+
+            //Search
+            DocumentSearchResult<BookModel> response = indexClient.Documents.SearchAsync<BookModel>(searchText, sp).Result;
+            foreach (SearchResult<BookModel> result in response.Results)
+            {
+                Console.WriteLine(result.Document);
+            }
+            if (response.Facets != null)
+            {
+                foreach (var facet in response.Facets)
+                {
+                    Console.WriteLine("Facet Name: " + facet.Key);
+                    foreach(var value in facet.Value)
+                    {
+                        Console.WriteLine("Value :" + value.Value + " - Count: " + value.Count);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AzureSearch/BookModel.cs
@@ -0,0 +1,62 @@
+﻿namespace AzureSearch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BookModel
+    {
+        public string ISBN { get; set; }
+        public string Titulo { get; set; }
+        public List<string> Autores { get; set; }
+        public DateTimeOffset FechaPublicacion { get; set; }
+        public string Categoria { get; set; }
+
+        public List<BookModel> GetBooks()
+        {
+            var listBooks = new List<BookModel>();
+            listBooks.Add(new BookModel()
+            {
+                ISBN = "9781430224792",
+                Titulo = "Windows Azure Platform (Expert's Voice in .NET)",
+                Categoria = "Comic",
+                Autores = new List<string>() { "Redkar", "Tejasw" },
+                FechaPublicacion = DateTimeOffset.Now.AddDays(-2)
+            });
+
+            listBooks.Add(new BookModel()
+            {
+                ISBN = "9780470506387",
+                Titulo = "Cloud Computing with the Windows Azure Platform",
+                Categoria = "Terror",
+                Autores = new List<string>() { "Roger Jennings" },
+                FechaPublicacion = DateTimeOffset.Now
+            });
+
+            listBooks.Add(new BookModel()
+            {
+                ISBN = "9780889222861",
+                Titulo = "Azure Blues",
+                Categoria = "Terror",
+                Autores = new List<string>() { "Gilbert", "Gerry" },                
+                FechaPublicacion = DateTimeOffset.Now.AddMonths(-3)
+            });
+
+            listBooks.Add(new BookModel()
+            {
+                ISBN = "9780735649675",
+                Titulo = "Moving Applications to the Cloud on the Microsoft Azure(TM) Platform",
+                Categoria = "Fiction",
+                Autores = new List<string>() { "Pace", "Eugenio Betts", "Dominic Densmore", "Scott; Dunn", "Ryan Narumoto", "Masashi Woloski", "Matias" },
+                FechaPublicacion = DateTimeOffset.Now
+            });
+
+            return listBooks;
+        }
+
+        public override string ToString()
+        {
+            return "ISBN: " + this.ISBN + " - titulo: " + this.Titulo + " - Autores: " + String.Join(", ", this.Autores);     
+        }
+    }
+}
